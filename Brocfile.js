3377c873293519285eabdb6633787a266a0a08c5
marco.demoja <markomoja@gmail.com>
2014-11-15 17:05:39 +0100

    compass compiler and sprites generation

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,16 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+	compassOptions: {
+    	outputStyle: 'expanded',
+    	relativeAssets: false,
+    	sassDir: 'app/styles/sass',
+    	// imagesDir: 'public/images',
+    	imagesDir: 'public/images',
+    	cssDir: 'dist/assets', 
+  	}
+}); 
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
