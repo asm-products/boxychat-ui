825ac8722732ba4942ddbad6fd9141d6ad29560d
Juan Corona <juan.carlos.corona.r@gmail.com>
2014-11-18 02:58:01 -0800

    Import the new dependencies.

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/lodash/dist/lodash.js');
+app.import('bower_components/socket.io-client/socket.io.js');
+app.import('bower_components/ember-sockets/package/EmberSockets.js');
+
 module.exports = app.toTree();
