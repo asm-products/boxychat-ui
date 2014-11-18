a9eeeb93fffba55f749419a564bb43af5b9b9bd0
marco.demoja <markomoja@gmail.com>
2014-11-18 23:41:02 +0100

    fixed hardcoded param

--- app/controllers/chatroom.js
+++ app/controllers/chatroom.js
@@ -7,7 +7,7 @@ export default Ember.ObjectController.extend({
 });
 
 $(window,document).on('load resize ready',function(){
-	var sidebarHeight = $(window).height()-$('#top-bar').height()-32;
+	var sidebarHeight = $(window).height()-$('#top-bar').height();
 	var messageboxWidth = $(window).width()-$('#side-bar').outerWidth();
 	$('#side-bar').css('height',sidebarHeight+'px');
 	$('#messagebox,.messagebar').css('width',messageboxWidth+'px');
